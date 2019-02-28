3fe0306e7da86fe208ddb62a79fdc5982b886e14
derushio <narumi18wa@gmail.com>
2019-02-28 18:57:23 +0900

    <master>[modified] 改行

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,9 @@ const isProduct = env.NODE_ENV == 'production';
 
 module.exports = {
     // 商用の場合はpathをきちんと設定したほうが良い
-    publicPath: isProduct ? '/' : './',
+    publicPath: isProduct
+        ? '/'
+        : './',
 
     css: {
         sourceMap: !isProduct,
