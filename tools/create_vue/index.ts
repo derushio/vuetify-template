db91eda0fc9ac505d79a6f2bb927ce279bb9197f
derushio <narumi18wa@gmail.com>
2019-04-16 12:31:08 +0900

    <master>[modified] help

--- tools/create_vue/index.ts
+++ tools/create_vue/index.ts
@@ -42,10 +42,10 @@ yargs
         type: 'string',
         required: true,
     })
-    .example('create:view --name `/path/to/view/ViewName`',
-        '`src/views/path/to/view/ViewName.vue` を生成します')
+    .example('create:view --name `path/to/view/ViewName`',
+        '`/src/views/path/to/view/ViewName.vue` を生成します')
     .example('create:component --name `path/to/component/ComponentName`',
-        '`src/components/path/to/component/ComponentName.vue` を生成します');
+        '`/src/components/path/to/component/ComponentName.vue` を生成します');
 const ARGS = yargs.parse(process.argv);
 
 // --------------------------------------------------
