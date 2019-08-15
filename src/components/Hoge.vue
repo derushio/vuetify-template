15cced2a731a8751cc663cf5c59741103b7cfc78
derushio <narumi18wa@gmail.com>
2019-08-15 11:46:25 +0900

    <master>[modified] fix command tool

---
+++ src/components/Hoge.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+.hoge
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Hoge extends Vue {}
+</script>
+
+<style lang='stylus' scoped>
+@require '~@/assets/styles/entry/_variable.styl';
+
+.hoge {}
+</style>
